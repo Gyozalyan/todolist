d3ade7c75d28694be5bb7b779040d19a162de22d
Gyozalyan <tgyozalyan7@gmail.com>
2023-03-16 20:21:15 +0400

    Lesson 64 fin

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,45 +1,57 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Input from 'react-bootstrap/InputGroup';
-import { Col, Form, FloatingLabel } from 'react-bootstrap';
-import React, { useState } from 'react';
-import {useRef} from 'react';
+import React, { useState } from 'react'
+import Input from 'react-bootstrap/InputGroup'
+import { Col, Form, Card, Button } from 'react-bootstrap'
+import { useRef } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
-
-export default function Task({taskObj,data, deleteTask,}){
-  const ref = useRef(null);
+export default function Task({ taskObj, data, deleteTask }) {
+  const ref = useRef(null)
 
   const [valueOfTask, setVal] = useState(data)
-  
-   let makeTextEditable = () =>{
-    ref.current.focus();
-   
 
+  let makeTextEditable = () => {
+    ref.current.focus()
   }
 
-
-return(
+  return (
     <Col lg={4} md={6} className="mt-5">
-    <Card style={{ width: '18rem' }} className="makeCenter" >
-      <Input className="mb-3">
-        <Input.Checkbox aria-label="Checkbox for following text input" className = 'inputCheckBox' />
-      </Input>
-    <Card.Body>
-    <FloatingLabel
-    id="foo"
-        controlId="floatingTextarea"
-        className="mb-3"
-      >
-        <Form.Control  ref={ref}  as="textarea" placeholder="Leave a comment here" value={valueOfTask} onChange ={(e)=>{setVal(e.target.value)}} />
-      </FloatingLabel>
-      <Button variant="outline-danger" className="float-end" onClick = {()=>{deleteTask(taskObj)}}>Delete</Button>
-      <Button variant="outline-success" className="float-end btns" onClick = {makeTextEditable}>Edit</Button>
-    
-    </Card.Body>
-  </Card>
-  </Col>
-)
-
+      <Card style={{ width: '18rem' }} className="makeCenter">
+        <Input className="mb-3">
+          <Input.Checkbox
+            aria-label="Checkbox for following text input"
+            className="inputCheckBox"
+          />
+        </Input>
+        <Card.Body>
+          <Form.Control
+            ref={ref}
+            as="textarea"
+            placeholder="Leave a comment here"
+            className="mb-3"
+            value={valueOfTask}
+            onChange={(e) => {
+              setVal(e.target.value)
+            }}
+          />
+          <Button
+            variant="outline-danger"
+            className="float-end"
+            onClick={() => {
+              deleteTask(taskObj)
+            }}
+          >
+            <FontAwesomeIcon icon={faTrashCan} />
+          </Button>
+          <Button
+            variant="outline-warning"
+            className="float-end btns"
+            onClick={makeTextEditable}
+          >
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
 }
-
- 
--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -1,59 +1,63 @@
-import { Component } from "react";
-import {Col, Container, Row, InputGroup, Form, Button} from "react-bootstrap";
-import Task from "../Components/Task";
-import { idGenerator } from "../Utils/Helper";
+import { Component } from 'react'
+import { Col, Container, Row, InputGroup, Form, Button } from 'react-bootstrap'
+import Task from '../Components/Task'
+import { idGenerator } from '../Utils/Helper'
 
 export default class ToDo extends Component {
   state = {
     tasks: [],
-    newText: "",
-
-  };
+    newText: '',
+  }
 
   getValue = (event) => {
     const newText = event.target.value.trim()
     this.setState({
       newText,
-    });
-
-
-  };
+    })
+  }
 
   addTemplate = () => {
-if(this.state.newText === ""){
-  return
-}
+    if (this.state.newText === '') {
+      return
+    }
 
-    const tasks = [...this.state.tasks];
+    const tasks = [...this.state.tasks]
     tasks.push({
       id: idGenerator(),
       text: this.state.newText,
-    });
+    })
 
     this.setState({
       tasks,
-      newText: "",
-    });
-  };
+      newText: '',
+    })
+  }
 
-  handleEvent =(event)=>{
-    if (event.key === 'Enter'){
+  handleEvent = (event) => {
+    if (event.key === 'Enter') {
       this.addTemplate()
     }
   }
 
-
-  deleteTask=(currentTask)=>{
-     this.setState({
-      tasks: this.state.tasks.filter((task)=>{return task!==currentTask})
+  deleteTask = (currentTask) => {
+    this.setState({
+      tasks: this.state.tasks.filter((task) => {
+        return task !== currentTask
+      }),
     })
   }
 
-
   render() {
     const taskJsx = this.state.tasks.map((task) => {
-      return <Task taskObj={task} data={task.text} key={task.id} deleteTask={this.deleteTask} />;
-    });
+      return (
+        <Task
+          taskObj={task}
+          data={task.text}
+          key={task.id}
+          deleteTask={this.deleteTask}
+        />
+      )
+    })
 
     const isButtonDisabled = !this.state.newText
 
@@ -72,7 +76,7 @@ if(this.state.newText === ""){
                 aria-describedby="basic-addon2"
                 onChange={this.getValue}
                 onKeyDown={this.handleEvent}
-                value = {this.state.newText}
+                value={this.state.newText}
               />
               <Button
                 variant="success"
@@ -87,6 +91,6 @@ if(this.state.newText === ""){
         </Row>
         <Row>{taskJsx}</Row>
       </Container>
-    );
+    )
   }
 }
