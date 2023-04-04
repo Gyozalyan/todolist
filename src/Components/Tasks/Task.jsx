10d42393e35e75d79c5513e98f8494916eea7067
Gyozalyan <tgyozalyan7@gmail.com>
2023-04-04 12:35:34 +0400

    memo for Task

--- src/Components/Tasks/Task.jsx
+++ src/Components/Tasks/Task.jsx
@@ -1,31 +1,27 @@
 import React, { useState } from "react";
 import styles from "./task.module.css";
-import { Col, Form, Card, Button } from "react-bootstrap";
-import { useRef } from "react";
+import { Form, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { memo } from "react";
 
-export default function Task({ data, deleteTask, selecteTasks, number }) {
+ function Task({ data, deleteTask, selecteTasks, number }) {
   return (
     <div className={styles.task}>
-      
       <div className={styles.taskBackground}>
-     
         <div className={styles.checkBody}>
-        <div> {data.date}</div>
-       
-        
+          <div> {data.date}</div>
+
           <Form.Check
             className={styles.selectTask}
             onClick={() => {
               selecteTasks(data.id);
             }}
           />
-             
+
           <div className={styles.taskContent}>
-    
             <strong>
-               {number}. {data.title}{" "}
+              {number}. {data.title}{" "}
             </strong>{" "}
             : {data.body}
           </div>
@@ -52,54 +48,5 @@ export default function Task({ data, deleteTask, selecteTasks, number }) {
       </div>
     </div>
   );
-
-  // const ref = useRef(null)
-
-  // const [valueOfTask, setVal] = useState(data.text)
-
-  // const makeTextEditable = () => {
-  //   ref.current.focus()
-  // }
-
-  // return (
-  //   <Col lg={4} md={6} className="mt-5">
-  //     <Card style={{ width: '18rem' }} className="makeCenter">
-  //        <Form.Check className = {styles.selectTask}
-  //         onClick={()=>{
-
-  //          selecteTasks(data.id)
-
-  //        }}/>
-
-  //       <Card.Body>
-  //         <Form.Control
-  //           ref={ref}
-  //           as="textarea"
-  //           placeholder="Leave a comment here"
-  //           className="mb-3"
-  //           value={valueOfTask}
-  //           onChange={(e) => {
-  //             setVal(e.target.value)
-  //           }}
-  //         />
-  //         <Button
-  //           variant="outline-danger"
-  //           className="float-end"
-  //           onClick={() => {
-  //             deleteTask(data.id)
-  //           }}
-  //         >
-  //           <FontAwesomeIcon icon={faTrashCan} />
-  //         </Button>
-  //         <Button
-  //           variant="outline-warning"
-  //           className="float-end btns"
-  //           onClick={makeTextEditable}
-  //         >
-  //           <FontAwesomeIcon icon={faPenToSquare} />
-  //         </Button>
-  //       </Card.Body>
-  //     </Card>
-  //   </Col>
-  // )
 }
+export default memo(Task)
