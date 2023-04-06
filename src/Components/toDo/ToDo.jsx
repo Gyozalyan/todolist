d8827672e0b30d772f849a80f9f27bce0f2939a1
Gyozalyan <tgyozalyan7@gmail.com>
2023-04-06 11:24:11 +0400

    fixed search and filter options

--- src/Components/toDo/ToDo.jsx
+++ src/Components/toDo/ToDo.jsx
@@ -7,41 +7,25 @@ import ConfirmDialog from "../ConfirmDialogDelete/ConfirmDialog";
 import MySelect from "../Select/MySelect";
 import { creationDate } from "../Date";
 
-export default function ToDo() { 
+export default function ToDo() {
+  const [tasks, setTasks] = useState([]);
+  const [newText, setNewText] = useState("");
+  const [body, setBody] = useState("");
+  const [selectedTasks, setSelectedTasks] = useState(new Set());
+  const [openModal, setOpenModal] = useState(false);
 
-  const [tasks, setTasks] = useState([])
-    const [newText, setNewText] = useState("")
-    const [body, setBody] = useState("")
-    const [selectedTasks, setSelectedTasks] = useState(new Set())
-    const [openModal, setOpenModal] = useState(false)
-    // const [filterTasksBy, setFilterTasksBy] = useState("")
-    // const [searchQuery, setSearchQuery] = useState("")
-
-
-    // getSearchQuery = (event) => {
-    //   const searchQuery = event.target.value;
-
-    //   this.setState({ 
-    //     searchQuery,
-    //     tasks: [...this.state.tasks].filter((task)=>{
-    //       return task.title.toLowerCase().includes(this.state.searchQuery.toLowerCase())})
-    //   });
-
-
-
-    //  };
+  const [filterTasksBy, setFilterTasksBy] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const getValueTitle = (event) => {
     const newText = event.target.value.trim();
 
-    setNewText(newText)
-
+    setNewText(newText);
   };
 
   const getValueBody = (event) => {
     const body = event.target.value.trim();
-    setBody(body)
-
+    setBody(body);
   };
 
   const addTemplate = () => {
@@ -57,11 +41,9 @@ export default function ToDo() {
       date: creationDate(),
     });
 
-
-    setTasks(tasksCopy)
-    setNewText("")
-    setBody("")
-
+    setTasks(tasksCopy);
+    setNewText("");
+    setBody("");
   };
 
   const handleEvent = (event) => {
@@ -71,11 +53,10 @@ export default function ToDo() {
   };
 
   const deleteTask = (id) => {
-
     const savedTasks = tasks.filter((task) => {
       return task.id !== id;
     });
-    setTasks(savedTasks)
+    setTasks(savedTasks);
 
     const updatedState = {
       tasks: savedTasks,
@@ -84,14 +65,11 @@ export default function ToDo() {
     if (selectedTasks.has(id)) {
       const selectedTasksCopy = new Set(selectedTasks);
       selectedTasksCopy.delete(id);
-      setSelectedTasks(updatedState)
+      setSelectedTasks(updatedState);
     }
-
-    ;
   };
 
   const deleteSelectedTasks = () => {
-
     const savedTasks = [];
 
     tasks.forEach((task) => {
@@ -100,11 +78,9 @@ export default function ToDo() {
       }
     });
 
-
-    setTasks(savedTasks)
-    setSelectedTasks(new Set())
-    setOpenModal(false)
-
+    setTasks(savedTasks);
+    setSelectedTasks(new Set());
+    setOpenModal(false);
   };
 
   const checkedTasks = (id) => {
@@ -112,132 +88,128 @@ export default function ToDo() {
     if (selectedTasksCopy.has(id)) {
       selectedTasksCopy.delete(id);
     } else {
-
-      setSelectedTasks(selectedTasksCopy.add(id))
-
+      setSelectedTasks(selectedTasksCopy.add(id));
     }
   };
 
   const isShown = () => {
-    setOpenModal(true)
-
+    setOpenModal(true);
   };
 
   const toCancel = () => {
-    setOpenModal(false)
+    setOpenModal(false);
   };
 
-  // const filterTasks = (sortby) => {
+  const FilterTasksBy = (sortby) => {
+    setFilterTasksBy(sortby);
 
-  //   this.setState({
-  //     tasks: this.state.tasks.sort((a, b) => {
-  //       return a[sortby].localeCompare(b[sortby]);
-  //     }),
-  //     filterTasksBy: sortby,
-  //   });
-  // };
+    setTasks(
+      tasks.sort((a, b) => {
+        return a[sortby].localeCompare(b[sortby]);
+      })
+    );
+  };
 
+  const getSearchQuery = (event) => {
+    setSearchQuery(event.target.value);
+    setTasks(
+      [...tasks].filter((task) => {
+        return task.title.toLowerCase().includes(searchQuery.toLowerCase());
+      })
+    );
+  };
 
- const taskJsx = tasks.map((task, index) => {
+  const taskJsx = tasks.map((task, index) => {
+    return (
+      <Task
+        data={task}
+        key={task.id}
+        deleteTask={deleteTask}
+        selecteTasks={checkedTasks}
+        number={index + 1}
+      />
+    );
+  });
+
   return (
-    <Task
-      data={task}
-      key={task.id}
-      deleteTask={deleteTask}
-      selecteTasks={checkedTasks}
-      number={index + 1}
-    />
-  );
- });
+    <Container>
+      <Row>
+        <Col className="heading mt-5">
+          <p className="text-center mt-4 fs-1">
+            Hello Tamara. What are we going to succeed today?
+          </p>
 
-   return (
-  <Container>
-    <Row>
-      <Col className="heading mt-5">
-        <p className="text-center mt-4 fs-1">
-          Hello Tamara. What are we going to succeed today?
-        </p>
+          <InputGroup className={styles.inputName}>
+            <Form.Control
+              type="text"
+              placeholder="Please type the name of the task"
+              aria-describedby="basic-addon2"
+              onChange={getValueTitle}
+              onKeyDown={handleEvent}
+              value={newText}
+            />
+          </InputGroup>
 
-        <InputGroup className={styles.inputName}>
-          <Form.Control
-            type="text"
-            placeholder="Please type the name of the task"
-            aria-describedby="basic-addon2"
-            onChange={getValueTitle}
-            onKeyDown={handleEvent}
-            value={newText}
-          />
-        </InputGroup>
+          <InputGroup className={styles.inputName}>
+            <Form.Control
+              type="text"
+              placeholder="Please type the description of the task"
+              aria-describedby="basic-addon2"
+              value={body}
+              onChange={getValueBody}
+            />
 
-        <InputGroup className={styles.inputName}>
-          <Form.Control
-            type="text"
-            placeholder="Please type the description of the task"
-            aria-describedby="basic-addon2"
-            value={body}
-            onChange={getValueBody}
-          />
+            <Button
+              variant="success"
+              id="button-addon2"
+              onClick={addTemplate}
+              disabled={!newText}
+            >
+              +Add
+            </Button>
+          </InputGroup>
 
+          <div className={styles.filterAndSearch}>
+            <MySelect
+              defaultValue="Filter tasks by"
+              filterBy={[
+                { name: "Name", value: "title" },
+                { name: "Description", value: "body" },
+                { name: "Date", value: "date" },
+              ]}
+              value={filterTasksBy}
+              onChange={FilterTasksBy}
+            />
+
+            <input
+              className={searchQuery}
+              placeholder="Search task..."
+              value={searchQuery}
+              onChange={getSearchQuery}
+            ></input>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <Button
-            variant="success"
-            id="button-addon2"
-            onClick={addTemplate}
-            disabled={!newText}
+            variant="danger"
+            className={styles.deleteselected}
+            onClick={isShown}
+            disabled={selectedTasks.size === 0}
           >
-            +Add
+            Delete Selected
           </Button>
-        </InputGroup>
+        </Col>
+      </Row>
 
-        {/* <div className={styles.filterAndSearch}> */}
-        {/* <MySelect */}
-        {/* defaultValue="Filter tasks by" */}
-        {/* filterBy={[ */}
-        {/* { name: "Name", value: "title" }, */}
-        {/* { name: "Description", value: "body" }, */}
-        {/* { name: "Date", value: "date" }, */}
-        {/* ]} */}
-        {/* value={this.state.filterTasksBy} */}
-        {/* onChange={this.filterTasks} */}
-        {/* /> */}
-        {/*  */}
-        {/*  */}
-        {/* <input */}
-        {/* className={styles.search} */}
-        {/* placeholder="Search task..." */}
-        {/* value={this.state.value} */}
-        {/* onChange={this.getSearchQuery}></input> */}
-        {/*  */}
-        {/*  */}
-        {/*  */}
-        {/* </div> */}
-
-
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <Button
-          variant="danger"
-          className={styles.deleteselected}
-          onClick={isShown}
-          disabled={selectedTasks.size === 0}
-        >
-          Delete Selected
-        </Button>
-      </Col>
-    </Row>
-
-    <Row>{taskJsx}</Row>
-    <ConfirmDialog
-      taskCount={selectedTasks.size}
-      isOpen={openModal}
-      confirmDelete={deleteSelectedTasks}
-      cancellation={toCancel}
-    />
-  </Container>
-);
+      <Row>{taskJsx}</Row>
+      <ConfirmDialog
+        taskCount={selectedTasks.size}
+        isOpen={openModal}
+        confirmDelete={deleteSelectedTasks}
+        cancellation={toCancel}
+      />
+    </Container>
+  );
 }
-
-    
-  
-   
