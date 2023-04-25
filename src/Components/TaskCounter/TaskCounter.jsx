c55a7e03dd6dcb5fa282e0c12f517806f5ef536b
Gyozalyan <tgyozalyan7@gmail.com>
2023-04-26 00:11:56 +0400

    functionality of task counter

--- src/Components/TaskCounter/TaskCounter.jsx
+++ src/Components/TaskCounter/TaskCounter.jsx
@@ -1,49 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import styles from'./TaskCounter.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from 'react'
+import styles from './TaskCounter.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { PropTypes } from "prop-types";
 
-import { faSpinner, faCheckDouble, faListUl } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSpinner,
+  faCheckDouble,
+  faListUl,
+} from '@fortawesome/free-solid-svg-icons'
+
+const TaskCounter = ({ tasks }) => {
+  const [tasksCounter, setTasksCounter] = useState({
+    doneTasks: 0,
+    activeTasks: 0,
+    allTasks: tasks.length,
+  })
 
 
-const NumberCounter = () => {
-    const [completeCount, setCompleteCount] = useState(0);
-    const [ongoingCount, setOngoingCount] = useState(0);
-    const [summaryCount, setSummaryCount] = useState(0);
-  
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCompleteCount((prevCount) => prevCount + 1);
-      }, 1000); // auto-increment complete counter every 1 second
-  
-      return () => {
-        clearInterval(intervalId);
-      };
-    }, []); // empty dependency array to run the effect only once on mount
-  
-    return (
-      <div className={styles.counterWidget}>
-        <div className= {styles.counterSection}>
+  useEffect(() => {
+    const doneTasks = []
+    const activeTasks = []
+
+    tasks.forEach((task) => {
+      if (task.status === 'done') {
+        doneTasks.push(task.status)
+      }
+
+      if (task.status === 'active') {
+        activeTasks.push(task.status)
+      }
+    })
+
+    setTasksCounter({
+      ...tasksCounter,
+      doneTasks: doneTasks.length,
+      activeTasks: activeTasks.length,
+      allTasks: tasks.length,
+    })
+    
+
+  }, [tasks]) 
+
+  return (
+    <div className={styles.counterWidget}>
+      <div className={styles.counterSection}>
         <FontAwesomeIcon icon={faCheckDouble} />
-        
-        <h1 id="completeCounter" className={styles.counterValue}>{completeCount}</h1>
+
+        <h1 id="completeCounter" className={styles.counterValue}>
+          {tasksCounter.doneTasks}
+        </h1>
         <h2 className={styles.counterTitle}>Completed</h2>
-         
-        </div>
-        <div  className= {styles.counterSection}>
-        <FontAwesomeIcon icon={faSpinner} spin /> 
-
-        <h1 id="ongoingCounter" className={styles.counterValue}>{ongoingCount}</h1>
-          <h2 className={styles.counterTitle}>Pending</h2>
-          
-        </div>
-        <div  className= {styles.counterSection}>
-        <FontAwesomeIcon icon={faListUl} />
-                <h1 id="summaryCounter" className={styles.counterValue}>{summaryCount}</h1>
-          <h2 className={styles.counterTitle}>Tasks</h2>
-          
-        </div>
       </div>
-    );
-};
+      <div className={styles.counterSection}>
+        <FontAwesomeIcon icon={faSpinner} spin />
 
-export default NumberCounter;
+        <h1 id="ongoingCounter" className={styles.counterValue}>
+        {tasksCounter.activeTasks}
+        </h1>
+        <h2 className={styles.counterTitle}>Pending</h2>
+      </div>
+      <div className={styles.counterSection}>
+        <FontAwesomeIcon icon={faListUl} />
+        <h1 id="summaryCounter" className={styles.counterValue}>
+        {tasksCounter.allTasks}
+        </h1>
+        <h2 className={styles.counterTitle}>Tasks</h2>
+      </div>
+    </div>
+  )
+}
+
+TaskCounter.propTypes = {
+    tasks: PropTypes.object.isRequired,
+  };
+
+export default TaskCounter
