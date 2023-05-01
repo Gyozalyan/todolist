af17fb40f06d28b58359f8681f3bd7516873aaa7
Gyozalyan <tgyozalyan7@gmail.com>
2023-05-01 12:55:02 +0400

    Moment

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,14 +1,6 @@
 import moment from 'moment';
 
 export const formatDate = (date) => {
-
-    if (date instanceof Date) {
-      return date.toISOString().slice(0, 10);
-    }
-    if (typeof date === "string") {
-      return date.slice(0, 10);
-    }
-
     if(!date){
       return ''
     }
